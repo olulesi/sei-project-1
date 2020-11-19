ca5e17a470d9a8e0c2b82faa30f8e845a6febaa7
tobil <tobilesi@gmail.com>
2020-11-19 23:53:36 +0000

    grid with background paper lines

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,28 @@
-console.log('we in this bitch')
+function init() {
+
+  // * Variables
+  //calls the grid container
+  const grid = document.querySelector('.grid')
+  
+  // var the width so you can manipulate the size whenever
+  const width = 12
+  const height = width * 2
+  const cellCount = height * width
+  const cells = []
+
+  // * Make a grid
+  // needs an argument for position
+  function createGrid() {
+    for (let i = 0; i < cellCount; i++) {
+      const cell = document.createElement('div')
+      //
+      grid.appendChild(cell)
+      cells.push(cell)
+    }
+  }
+
+  createGrid()
+ 
+}
+
+window.addEventListener('DOMContentLoaded', init)
