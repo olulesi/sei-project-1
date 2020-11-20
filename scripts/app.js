3ae8be381547580bede75d363550fe7711883bcd
tobil <tobilesi@gmail.com>
2020-11-20 11:35:00 +0000

    grid with shape grid aside squared 4x4

--- scripts/app.js
+++ scripts/app.js
@@ -3,12 +3,15 @@ function init() {
   // * Variables
   //calls the grid container
   const grid = document.querySelector('.grid')
-  
+  const tetrisShape = document.querySelector('.shape')
   // var the width so you can manipulate the size whenever
-  const width = 12
-  const height = width * 2
-  const cellCount = height * width
+  const gridWidth = 12
+  const gridHeight = gridWidth * 2
+  const cellCount = gridHeight *  gridWidth
   const cells = []
+  const shapeWidth = 4
+  const shapeCellCount = shapeWidth * shapeWidth 
+  const shapeCells = []
 
   // * Make a grid
   // needs an argument for position
@@ -22,7 +25,18 @@ function init() {
     }
   }
 
+  function tetrisShapeGrid() {
+    for (let i = 0; i < shapeCellCount; i++) {
+      const shapeCell = document.createElement('div')
+      // cell.textContent = i
+      //
+      tetrisShape.appendChild(shapeCell)
+      shapeCells.push(shapeCell)
+    }
+  }
+
   createGrid()
+  tetrisShapeGrid()
  
 }
 
